f6a2bc6aaff1855287d4770305bfb35605c76557
DevThive <thive8564@gmail.com>
2024-02-25 23:27:53 +0900

    add

--- knori-front/.next/server/app/socials/page.js
+++ knori-front/.next/server/app/socials/page.js
@@ -222,6 +222,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ 5130:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7734, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8709, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 9150, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2698, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 7833, 23))
+
+/***/ }),
+
 /***/ 6911:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
@@ -258,7 +269,7 @@ module.exports = __webpack_require__(7538);
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [575,330,982], () => (__webpack_exec__(5446)));
+var __webpack_exports__ = __webpack_require__.X(0, [575,405,982], () => (__webpack_exec__(5446)));
 module.exports = __webpack_exports__;
 
 })();
