6a9c252a35a94b7bde81108a8071dd2502436376
DevThive <thive8564@gmail.com>
2024-02-28 03:23:22 +0900

    update gitignore (.next)

--- knori-front/.next/static/chunks/app/socials/page-31c4421d83f2a500.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[382],{3743:function(n,e,u){Promise.resolve().then(u.t.bind(u,7095,23))}},function(n){n.O(0,[95,253,698,744],function(){return n(n.s=3743)}),_N_E=n.O()}]);
