bc6f6cb6fb2d39759b5d1a970965a81f495dea7b
DevThive <thive8564@gmail.com>
2024-05-08 01:21:21 +0900

    Update contact.service.ts

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -131,10 +131,13 @@ export class ContactService {
     }
 
     // bcrypt.compare() 함수를 사용하여 입력된 비밀번호와 저장된 해시를 비교합니다.
+    // 여기서는 사용자가 입력한 평문 비밀번호와 데이터베이스에 저장된 해시된 비밀번호를 직접 비교합니다.
     const isMatch = await bcrypt.compare(
       contactpassword.password,
       contact.password,
     );
+
+    // console.log(isMatch);
 
     if (!isMatch) {
       throw new BadRequestException('비밀번호가 올바르지 않습니다.');
