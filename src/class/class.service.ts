ae591d86230ba1fca88788c2e99071c31ba64f81
DevThive <thive8564@gmail.com>
2024-06-04 19:33:41 +0900

    클래스 가격 수정 기능 추가

--- src/class/class.service.ts
+++ src/class/class.service.ts
@@ -13,6 +13,7 @@ import { HideClassDto } from './dto/hide-class';
 import { v4 as uuidv4 } from 'uuid';
 import { UpdateClassScheduleDto } from './dto/update-schedule';
 import { ClassSchedule } from 'src/entity/class-schedule.entity';
+import { UpdateClassPriceDto } from './dto/update-price';
 @Injectable()
 export class ClassService {
   constructor(
@@ -82,6 +83,30 @@ export class ClassService {
     await this.classScheduleRepository.save(classSchedule18);
 
     return Class;
+  }
+
+  //클래스 가격 수정
+  async updateClassPrice(
+    updatePriceDto: UpdateClassPriceDto,
+    userId: number,
+    classId: number,
+  ) {
+    const user = await this.userService.findUserById(userId);
+
+    if (user.role !== 1) {
+      throw new BadRequestException('관리자만 수정이 가능합니다.');
+    }
+
+    const Class = this.findclassbyid(classId);
+    if (!Class) {
+      throw new BadRequestException('해당 클래스가 존재하지 않습니다.');
+    }
+
+    const updatedPrice = await this.classRepository.update(
+      { id: classId },
+      { price: updatePriceDto.price },
+    );
+    return updatedPrice;
   }
 
   //클래스 이미지 수정
